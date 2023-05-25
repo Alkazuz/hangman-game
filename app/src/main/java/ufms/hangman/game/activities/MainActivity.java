033e74e29bfaaf8f74465bb1fd0786ac58f0d50c
Alkazuz <kerallis2015@gmail.com>
2023-05-25 20:53:01 -0300

    feat: acvitity principal para seleção de nick e avatar

---
+++ app/src/main/java/ufms/hangman/game/activities/MainActivity.java
@@ -0,0 +1,50 @@
+package ufms.hangman.game.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import ufms.hangman.game.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    CircleImageView playerPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        createButtonPhotoHandle();
+    }
+
+    private void createButtonPhotoHandle() {
+        playerPhoto = findViewById(R.id.player_photo);
+        Button takePictureButton = findViewById(R.id.select_photo);
+
+        takePictureButton.setOnClickListener(v -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            playerPhoto.setImageBitmap(imageBitmap);
+        }
+    }
+
+}
