8b22682cfcd806a2fc16ec352d5a2cd1403d6a0d
Alkazuz <kerallis2015@gmail.com>
2023-06-01 21:56:06 -0300

    feat: adicinando persistencia de objetos

--- app/src/main/java/ufms/hangman/game/utils/LocalStorageManager.java
+++ app/src/main/java/ufms/hangman/game/utils/LocalStorageManager.java
@@ -2,6 +2,8 @@ package ufms.hangman.game.utils;
 
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+
 public class LocalStorageManager {
     private static final String PREF_NAME = "ufms.hangman.game.PREF_NAME";
 
@@ -15,6 +17,11 @@ public class LocalStorageManager {
         return instance;
     }
 
+    public void save(String key, Object value) {
+        String json = new Gson().toJson(value);
+        save(key, json);
+    }
+
     public void save(String key, String value) {
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString(key, value);
@@ -23,6 +30,14 @@ public class LocalStorageManager {
 
     public String get(String key) {
         return sharedPreferences.getString(key, null);
+    }
+
+    public Object loadObject(String key) {
+        String json = get(key);
+        if (json == null) {
+            return null;
+        }
+        return new Gson().fromJson(json, Object.class);
     }
 
     public void remove(String key) {
